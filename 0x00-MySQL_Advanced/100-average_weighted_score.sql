d56da40dd402b6fd2a4ffc0e1a8ede5f999f9af7
TSOlami <tijani.saheed@yahoo.com>
2023-08-10 20:17:19 +0100

    fix indentation in task 100

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,6 +1,6 @@
--- SQL script that creates a stored procedure
--- ComputeAverageWeightedScoreForUser that computes
--- and store the average weighted score for a student.
+-- A SQL script that creates a stored procedure
+-- ComputeAverageWeightedScoreForUser that computes and
+-- Store the average weighted score for a student.
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser (user_id INT)
